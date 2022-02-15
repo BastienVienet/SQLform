d1f9c17cb50fb81d6826c78253ac356aaa83c30b
Bastien Vienet <bastien.vienet@jobtrek.ch>
2022-02-15 17:03:24 +0100

    Implementing Bulma and making cards for users

--- list_users.php
+++ list_users.php
@@ -18,8 +18,49 @@ INNER JOIN countries c
 ');
 
 $stmt_all_data->execute([]);
-$user_all_data = $stmt_all_data->fetchAll();
+// $user_all_data = $stmt_all_data->fetchAll();
 
- foreach ($user_all_data as $row) {
+/*  foreach ($user_all_data as $row) {
     echo "{$row['first_name']} - {$row['last_name']} - {$row['birthdate']} - {$row['email']} - {$row['phone']} - {$row['civility']} - {$row['sex']} - {$row['street']} - {$row['postal_code']} - {$row['city']} - {$row['country_name']}<br><br>";
-}
+}*/
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1">
+    <title>Testing php and db interactions</title>
+    <link rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+</head>
+<body>
+
+<div class="columns is-multiline is-variable">
+    <?php while ($rUAD = $stmt_all_data->fetch()) { ?>
+        <div class="card is-child box column is-half">
+            <div class="card-content">
+                <div class="media">
+                    <div class="media-left">
+                        <figure class="image is-48x48">
+                            <img src="https://bulma.io/images/placeholders/96x96.png"
+                                 alt="Placeholder image">
+                        </figure>
+                    </div>
+                    <div class="media-content">
+                        <p class="title is-4"> <?= $rUAD['first_name'] . " " . $rUAD['last_name'] ?></p>
+                        <p class="subtitle is-6"><?= $rUAD['email'] ?></p>
+                    </div>
+                </div>
+                <div class="content">
+                    <p>Address
+                       : <?= $rUAD['street'] . " / " . $rUAD['postal_code'] . " " . $rUAD['city'] . " / " . $rUAD['country_name'] ?></p>
+                    <p>Phone number : <?= $rUAD['phone'] ?></p>
+                </div>
+            </div>
+        </div>
+    <?php } ?>
+</div>
+</body>
+</html>
