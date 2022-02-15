2562745c0e3c8b5b2826e7253b20bf693c3f1976
Bastien Vienet <bastien.vienet@jobtrek.ch>
2022-02-15 17:03:55 +0100

    Small changes

--- index.php
+++ index.php
@@ -92,132 +92,138 @@ if (isset($_POST['submit'])) {
 </head>
 <body>
 <section class="section">
+    <a href="list_users.php">See the users page with their data >></a>
     <div class="container">
-        <h1 class="title">
+        <h1 class="title is-1">
             Testing php and databases interactions with PDO </h1>
-        <h2 class="subtitle">Notice : All of these data are going to be collected to my computer</h2>
-        <a href="list_users.php">See the users page with their data >></a>
-        <form action="index.php"
-              method="post">
-            <h3 class="title is-3">User Table</h3>
-            <label for="first_name">First name<br>
-                <input class="input is-normal"
-                       type="text"
-                       name="first_name"
-                       placeholder="Enter your first name"
-                       required><br><br>
-            </label>
-            <label for="last_name">Last name<br>
-                <input class="input is-normal"
-                       type="text"
-                       name="last_name"
-                       placeholder="Enter your last name"
-                       required><br><br>
-            </label>
-            <label for="birthdate">Birthdate<br>
-                <input class="input is-normal"
-                       type="date"
-                       name="birthdate"
-                       placeholder="Enter your birthdate"
-                       required><br><br>
-            </label>
-            <label for="email">Email<br>
-                <input class="input is-normal"
-                       type="email"
-                       name="email"
-                       placeholder="Enter your email"
-                       required><br><br>
-            </label>
-            <label for="phone">Phone number<br>
-                <input class="input is-normal"
-                       type="tel"
-                       name="phone"
-                       maxlength="10"
-                       placeholder="Enter your phone number"><br>
-            </label><br>
-            <p>Civility</p>
-            <div>
-                <input id="civility"
-                       type="radio"
-                       name="civility"
-                       value="1"
-                       checked>
-                <label class="radio"
-                       for="civility">Single</label>
-                <input id="civility"
-                       type="radio"
-                       name="civility"
-                       value="2">
-                <label class="radio"
-                       for="civility">Married</label>
-                <input id="civility"
-                       type="radio"
-                       name="civility"
-                       value="3">
-                <label class="radio"
-                       for="civility">Divorced</label>
-            </div><br>
-            <p>Gender</p>
-            <div>
-                <input id="sex"
-                       type="radio"
-                       name="sex"
-                       value="1"
-                       checked>
-                <label class="radio"
-                       for="sex">Male
-                </label>
-                <input id="sex"
-                       type="radio"
-                       name="sex"
-                       value="2">
-                <label class="radio"
-                       for="sex">Female
-                </label>
-                <input id="sex"
-                       type="radio"
-                       name="sex"
-                       value="3">
-                <label class="radio"
-                       for="sex">Other
-                </label>
-            </div><br>
-            <h3 class="title is-3">Address table</h3>
-            <label for="country">Country<br>
-                <input class="input is-normal"
-                       type="text"
-                       name="country"
-                       placeholder="Enter your country"
-                       required><br><br>
-            </label>
-            <label for="street">Street<br>
-                <input class="input is-normal"
-                       type="text"
-                       name="street"
-                       placeholder="Enter your street"
-                       required><br><br>
-            </label>
-            <label for="postal_code">Postal code<br>
-                <input class="input is-normal"
-                       type="number"
-                       min="1000"
-                       max="9999"
-                       name="postal_code"
-                       placeholder="NPA"><br><br>
-            </label>
-            <label for="city">City<br>
-                <input class="input is-normal"
-                       type="text"
-                       name="city"
-                       placeholder="Enter your city"
-                       required><br><br>
-            </label>
-            <input class="button is-primary is-outlined is-light"
-                   id="submit_id"
-                   type="submit"
-                   name="submit"
-                   value="submit"><br><br><br>
-        </form>
+        <h2 class="subtitle">Notice : All of these data are going to be collected to my computer</h2><br>
+        <div class="columns is-multiline">
+            <div class="is-child box column is-8 is-offset-2">
+                <form action="index.php"
+                      method="post">
+                    <h3 class="title is-3">User Table</h3>
+                    <label for="first_name">First name<br>
+                        <input class="input is-normal"
+                               type="text"
+                               name="first_name"
+                               placeholder="Enter your first name"
+                               required><br><br>
+                    </label>
+                    <label for="last_name">Last name<br>
+                        <input class="input is-normal"
+                               type="text"
+                               name="last_name"
+                               placeholder="Enter your last name"
+                               required><br><br>
+                    </label>
+                    <label for="birthdate">Birthdate<br>
+                        <input class="input is-normal"
+                               type="date"
+                               name="birthdate"
+                               placeholder="Enter your birthdate"
+                               required><br><br>
+                    </label>
+                    <label for="email">Email<br>
+                        <input class="input is-normal"
+                               type="email"
+                               name="email"
+                               placeholder="Enter your email"
+                               required><br><br>
+                    </label>
+                    <label for="phone">Phone number<br>
+                        <input class="input is-normal"
+                               type="tel"
+                               name="phone"
+                               maxlength="10"
+                               placeholder="Enter your phone number"><br>
+                    </label><br>
+                    <p>Civility</p>
+                    <div>
+                        <input id="civility"
+                               type="radio"
+                               name="civility"
+                               value="1"
+                               checked>
+                        <label class="radio"
+                               for="civility">Single</label>
+                        <input id="civility"
+                               type="radio"
+                               name="civility"
+                               value="2">
+                        <label class="radio"
+                               for="civility">Married</label>
+                        <input id="civility"
+                               type="radio"
+                               name="civility"
+                               value="3">
+                        <label class="radio"
+                               for="civility">Divorced</label>
+                    </div>
+                    <br>
+                    <p>Gender</p>
+                    <div>
+                        <input id="sex"
+                               type="radio"
+                               name="sex"
+                               value="1"
+                               checked>
+                        <label class="radio"
+                               for="sex">Male
+                        </label>
+                        <input id="sex"
+                               type="radio"
+                               name="sex"
+                               value="2">
+                        <label class="radio"
+                               for="sex">Female
+                        </label>
+                        <input id="sex"
+                               type="radio"
+                               name="sex"
+                               value="3">
+                        <label class="radio"
+                               for="sex">Other
+                        </label>
+                    </div>
+                    <br>
+                    <h3 class="title is-3">Address table</h3>
+                    <label for="country">Country<br>
+                        <input class="input is-normal"
+                               type="text"
+                               name="country"
+                               placeholder="Enter your country"
+                               required><br><br>
+                    </label>
+                    <label for="street">Street<br>
+                        <input class="input is-normal"
+                               type="text"
+                               name="street"
+                               placeholder="Enter your street"
+                               required><br><br>
+                    </label>
+                    <label for="postal_code">Postal code<br>
+                        <input class="input is-normal"
+                               type="number"
+                               min="1000"
+                               max="9999"
+                               name="postal_code"
+                               placeholder="NPA"><br><br>
+                    </label>
+                    <label for="city">City<br>
+                        <input class="input is-normal"
+                               type="text"
+                               name="city"
+                               placeholder="Enter your city"
+                               required><br><br>
+                    </label>
+                    <input class="button is-primary is-outlined is-light"
+                           id="submit_id"
+                           type="submit"
+                           name="submit"
+                           value="submit"><br><br><br>
+                </form>
+            </div>
+        </div>
     </div>
 </section>
 </body>
